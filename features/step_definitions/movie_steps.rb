aaea4fb94f65340ffc0380c47a12f9b9664d2810
Adrian PPG <adrianppg3@yahoo.es>
2012-03-18 21:39:27 +0000

    push hacia heroku

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,10 +2,11 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+    assert_not_nil Movie.find_by_title movie[:title]
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  assert false, "Unimplmemented"
+#assert false, "Unimplmemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -25,4 +26,8 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+end
+
+Given /^(?:|I )am on (.+)$/ do |page_name|
+  visit "http://high-rain-8756.herokuapp.com/"
 end
