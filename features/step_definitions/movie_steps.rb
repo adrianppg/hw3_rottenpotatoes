914540bf4d82d2b0d454f76f3ed13af3fc30f4f3
Adrian PPG <adrianppg3@yahoo.es>
2012-03-20 17:05:20 +0000

    trying_to_modify_1

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -31,6 +31,7 @@ peneliculas = Movie.all_ratings.to_s.match(/[((,".")\)(,".")*]/)
 peneliculas = /[((,".")\)(,".")*]/.match(Movie.all_ratings.to_s)
 puts peneliculas.class
 puts /[^((, \[\] ".")\)(,".")*]/ =~ Movie.all_ratings.to_s
+  puts "joder"
   puts $1
   puts movida
 joderse = Movie.all_ratings.to_s
