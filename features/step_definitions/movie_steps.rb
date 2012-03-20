54f4d10343d4927de96b0e8dd54811892a71488d
Adrian PPG <adrianppg3@yahoo.es>
2012-03-20 17:01:24 +0000

    trying_to_modify_1

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -30,7 +30,7 @@ peneliculas = Movie.all_ratings.to_s.split(/[((,".")\)(,".")*]/)
 peneliculas = Movie.all_ratings.to_s.match(/[((,".")\)(,".")*]/)
 peneliculas = /[((,".")\)(,".")*]/.match(Movie.all_ratings.to_s)
 puts peneliculas.class
-movida =~ /[^((, \[\] ".")\)(,".")*]/.match(Movie.all_ratings.to_s)
+puts /[^((, \[\] ".")\)(,".")*]/ =~ Movie.all_ratings.to_s
   puts $1
   puts movida
 joderse = Movie.all_ratings.to_s
