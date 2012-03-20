a84f4183ca4a597030da89a643864c55bef29af1
Adrian PPG <adrianppg3@yahoo.es>
2012-03-20 16:05:53 +0000

    trying_to_modify_1

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -30,7 +30,7 @@ peneliculas = Movie.all_ratings.to_s.split(/[((,".")\)(,".")*]/)
     step %Q{I should see "#{x[:title]}" before "#{y[:title]}"}
 #step "I check the following ratings: " + Movie.all_ratings.to_s.split(/[((,".")\)(,".")*]/)
 #  step "I check the following ratings: " + Movie.all_ratings.to_s
-  step %Q{I check the following ratings: #{peneliculas}}"
+  step %Q{I check the following ratings: #{peneliculas}}
 end
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
