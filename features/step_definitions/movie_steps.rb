a5bdd98db1f2f83b0a01bde9437b7829e1767193
Adrian PPG <adrianppg3@yahoo.es>
2012-03-21 09:20:28 +0000

    trying_to_modify_1

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -5,7 +5,8 @@ Given /the following movies exist/ do |movies_table|
 #debugger
   movies_table.hashes.each do |movie|
 #   assert_not_nil Movie.find_by_title movie[:title]
-    page.has_content?(movie[:title])
+#page.has_content?(movie[:title])
+  print page.html
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
