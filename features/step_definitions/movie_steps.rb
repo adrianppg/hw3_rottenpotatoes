04b762e1b9894a2ace589574336c3312de91e400
Adrian PPG <adrianppg3@yahoo.es>
2012-03-22 12:57:57 +0000

    trying to order

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -39,12 +39,14 @@ Given /^(?:|I )am on (.+)$/ do |page_name|
 end
 
 Then /I should see movies sorted by (.*)/ do |sort_by_key|
+  puts sort_by_key.to_sym
+  puts sort_by_key
   moviesList = Movie.order(sort_by_key.to_sym)
 #moviesList.each do |movie|
 #    puts movie[:title]
 #  end
   moviesList[1..moviesList.length-1].zip(moviesList[0..moviesList.length-2]).each do |x, y|
-   print %Q{I should see "#{x[sort_by_key.to_sym]}" before "#{y[sort_by_key.to_sym]}"}
+    print %Q{I should see "#{x[sort_by_key.to_sym]}" before "#{y[sort_by_key.to_sym]}"}
     step %Q{I should see "#{x[sort_by_key.to_sym]}" before "#{y[sort_by_key.to_sym]}"}
   end
 end
