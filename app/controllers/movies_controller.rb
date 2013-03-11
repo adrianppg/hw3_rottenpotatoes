df08c4ca6289c13b409f7a4c16cc9269da6318ea
adrianppg <adrianppg@gmail.com>
2013-03-11 02:52:57 -0700

    tests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
 
-
+#movida 
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
